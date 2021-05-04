ed4a0e79ff4a9de34706c159fb69aee7b607bac9
lucasherfort <lucas.herfort@gmail.com>
2021-05-04 11:49:39 +0200

    Update project

---
+++ Assets/Scripts/Managers/VRManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRManager : MonoBehaviour
+{
+    [SerializeField]
+    private bool EnabledVR = true;
+
+    private void Start()
+    {
+        UnityEngine.XR.XRSettings.enabled = EnabledVR;
+    }
+}
