f62a0e2e35db4ce2f213b0e77edb5caa3cb806af
lucasherfort <lucas.herfort@gmail.com>
2021-05-17 11:44:03 +0200

    Add Noise

--- Assets/Scripts/Noise/TextureCreator.cs
+++ Assets/Scripts/Noise/TextureCreator.cs
@@ -6,17 +6,59 @@ public class TextureCreator : MonoBehaviour
     [Range(2, 512)]
     private int resolution = 256;
 
+    [SerializeField]
+    private bool mipmap = false;
+
+    [SerializeField]
+    private FilterMode filterMode;
+
+    [SerializeField]
+    [Range(1, 16)]
+    private int anisoLevel = 9;
+
+    [SerializeField]
+    private float frequency = 1f;
+
+    [Range(1, 3)]
+    public int dimensions = 3;
+
+    public NoiseMethodType type;
+
+    [Range(1, 8)]
+    public int octaves = 1;
+
+    [Range(1f, 4f)]
+    public float lacunarity = 2f;
+
+    [Range(0f, 1f)]
+    public float persistence = 0.5f;
+
+    [SerializeField]
+    private Gradient coloring;
+
     private Texture2D texture;
 
     private void OnEnable()
     {
-        texture = new Texture2D(resolution, resolution, TextureFormat.RGB24, true);
-        texture.name = "Procedural Texture";
-        texture.wrapMode = TextureWrapMode.Clamp;
-        //texture.filterMode = FilterMode.Point;
-        texture.filterMode = FilterMode.Bilinear;
-        GetComponent<MeshRenderer>().material.mainTexture = texture;
+        if(texture == null)
+        {
+            texture = new Texture2D(resolution, resolution, TextureFormat.RGB24, mipmap);
+            texture.name = "Procedural Texture";
+            texture.wrapMode = TextureWrapMode.Clamp;
+            texture.filterMode = filterMode;
+            texture.anisoLevel = anisoLevel;
+            GetComponent<MeshRenderer>().material.mainTexture = texture;
+        }
         FillTexture();
+    }
+
+    private void Update()
+    {
+        if (transform.hasChanged)
+        {
+            transform.hasChanged = false;
+            FillTexture();
+        }
     }
 
     public void FillTexture()
@@ -26,13 +68,30 @@ public class TextureCreator : MonoBehaviour
             texture.Resize(resolution, resolution);
         }
 
+        Vector3 point00 = transform.TransformPoint(new Vector3(-0.5f, -0.5f));
+        Vector3 point10 = transform.TransformPoint(new Vector3(0.5f, -0.5f));
+        Vector3 point01 = transform.TransformPoint(new Vector3(-0.5f, 0.5f));
+        Vector3 point11 = transform.TransformPoint(new Vector3(0.5f, 0.5f));
+
+        NoiseMethod method = Noise.noiseMethods[(int)type][dimensions - 1];
         float stepSize = 1f / resolution;
+        Random.InitState(42);
         for (int y = 0; y < resolution; y++)
         {
+            Vector3 point0 = Vector3.Lerp(point00, point01, (y + 0.5f) * stepSize);
+            Vector3 point1 = Vector3.Lerp(point10, point11, (y + 0.5f) * stepSize);
+
             for (int x = 0; x < resolution; x++)
             {
-                //texture.SetPixel(x, y, new Color((x + 0.5f) * stepSize, (y + 0.5f) * stepSize, 0f));
-                texture.SetPixel(x, y, new Color((x + 0.5f) * stepSize % 0.1f, (y + 0.5f) * stepSize % 0.1f, 0f) * 10f);
+                Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) * stepSize);
+
+                float sample = Noise.Sum(method, point, frequency, octaves, lacunarity, persistence);
+                if (type != NoiseMethodType.Value)
+                {
+                    sample = sample * 0.5f + 0.5f;
+                }
+
+                texture.SetPixel(x, y, coloring.Evaluate(sample));
             }
         }
         texture.Apply();
