40a9876d2e1443232e50128c67a29d7e2d3df882
lucasherfort <lucas.herfort@gmail.com>
2021-05-21 17:48:53 +0200

    Started include needle

---
+++ Assets/Scripts/Needle/NeedleInsertion.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class NeedleInsertion : MonoBehaviour
+{
+    private HapticPlugin[] devices;
+    private HapticPlugin sondeDevice;
+    private HapticPlugin needleDevice;
+
+    [SerializeField]
+    GameObject NeedlePointer;
+    [SerializeField]
+    GameObject echoPointer;
+
+    private void Start()
+    {
+        devices = (HapticPlugin[])FindObjectsOfType(typeof(HapticPlugin));
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+            if (devices[i].hapticManipulator == NeedlePointer)
+            {
+                needleDevice = devices[i];         
+            }
+            else if (devices[i].hapticManipulator == echoPointer)
+            {
+                sondeDevice = devices[i];
+
+            }
+        }
+        sondeDevice.shapesEnabled = true;
+        needleDevice.shapesEnabled = true;
+    }
+}
