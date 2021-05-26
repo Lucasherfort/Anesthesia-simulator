fdea9cf83d1b754299eade80a8174f587ccd35a8
lucasherfort <lucas.herfort@gmail.com>
2021-05-26 16:25:06 +0200

    Improve screen render

---
+++ Assets/Scripts/Probe/HapticProbe/HapticProbe.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class HapticProbe : MonoBehaviour
+{
+    [SerializeField]
+    private Material composite = null;
+    [SerializeField]
+    private Material ScreenPlaceHolder = null;
+
+    [SerializeField]
+    private GameObject Screenrenderer = null;
+
+    private void Start()
+    {
+        Screenrenderer.GetComponent<Renderer>().material = ScreenPlaceHolder;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.gameObject.tag == "Skin")
+        {
+            Screenrenderer.GetComponent<Renderer>().material = composite;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.gameObject.tag == "Skin")
+        {
+            Screenrenderer.GetComponent<Renderer>().material = ScreenPlaceHolder;
+        }
+    }
+}
