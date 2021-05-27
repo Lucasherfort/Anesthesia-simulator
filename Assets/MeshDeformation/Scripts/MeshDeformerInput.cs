6b96a3477f9664f7fbbd1e76d36e7ea1586c77e0
lucasherfort <lucas.herfort@gmail.com>
2021-05-27 16:30:46 +0200

    Start deforme mesh

---
+++ Assets/MeshDeformation/Scripts/MeshDeformerInput.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class MeshDeformerInput : MonoBehaviour
+{
+	
+	public float force = 10f;
+	public float forceOffset = 0.1f;
+	
+	void Update ()
+    {
+		if (Input.GetMouseButton(0))
+        {
+            Debug.Log("1");
+			HandleInput();
+		}
+	}
+
+	void HandleInput ()
+    {
+		Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+        Debug.Log("2");
+
+        if (Physics.Raycast(inputRay, out hit))
+        {
+            Debug.Log("3");
+            Debug.Log(hit.transform.name);
+            MeshDeformer deformer = hit.collider.GetComponent<MeshDeformer>();
+			if (deformer)
+            {
+                Debug.Log("4");
+                Vector3 point = hit.point;
+				point += hit.normal * forceOffset;
+                deformer.AddDeformingForce(point, force);
+            }
+		}
+	}
+}
