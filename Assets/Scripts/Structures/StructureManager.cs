6d527fae2373aebf2c50b3498e1defeab442f07e
lucasherfort <lucas.herfort@gmail.com>
2021-07-01 10:58:49 +0200

    Add StructureManager script

---
+++ Assets/Scripts/Structures/StructureManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public enum Structure
+{
+    Artery,
+    Veine,
+    Nerve
+}
+
+public class StructureManager : MonoBehaviour
+{
+    [SerializeField]
+    private Structure CurrentStructure;
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Needle")
+        {
+            Debug.Log(CurrentStructure+" a été touché");
+        }
+    }
+}
