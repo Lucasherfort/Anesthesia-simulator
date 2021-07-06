8eefb75ae757f0d54d5f23b53ff9b2c09e7d376f
lucasherfort <lucas.herfort@gmail.com>
2021-07-06 12:16:49 +0200

    Improve echographe screen

---
+++ Assets/Scripts/UI/CanvasEchographe.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+
+public class CanvasEchographe : MonoBehaviour
+{
+    [SerializeField]
+    private TMPro.TMP_Text DateAndHourTxt;
+
+    [SerializeField]
+    private TMPro.TMP_Text ZoomTxt;
+
+    [SerializeField]
+    private ZoomScreen zoomScreen;
+
+    private void Update()
+    {
+        DateAndHourTxt.text = System.DateTime.Now.ToString();
+        ZoomTxt.text = "Zoom : "+zoomScreen.currentZoom.ToString();
+    }
+}
