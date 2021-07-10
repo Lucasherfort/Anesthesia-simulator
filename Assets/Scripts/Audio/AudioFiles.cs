854f388101f15a5bba979be07e30b6778709c7e3
lucasherfort <lucas.herfort@gmail.com>
2021-07-10 20:20:09 +0200

    Add audio box

---
+++ Assets/Scripts/Audio/AudioFiles.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AudioFiles", menuName = "Audio/AudioFiles", order = 2)]
+public class AudioFiles : ScriptableObject
+{
+    [Header("Ambiance")]
+    [SerializeField]
+    private SoundLoopClip roomSoundEffect = null;
+ 
+    public SoundOneShotClip SoundOneShotToClip (SoundOneShot sound) 
+    {
+        switch (sound) 
+        {
+            default : 
+                Debug.LogError("SoundOneShotClip : " + sound + " was not found!");
+                return null;
+        }
+    }
+
+    public SoundLoopClip SoundLoopToClip (SoundLoop sound) 
+    {
+        switch (sound) 
+        {
+            case SoundLoop.RoomSoundEffect : return roomSoundEffect;
+
+            default : 
+                Debug.LogError("SoundLoopClip : " + sound + " was not found!");
+                return null;
+        }
+    }
+}
+
+public enum SoundOneShot 
+{
+
+}
+
+public enum SoundLoop 
+{
+    RoomSoundEffect
+}
---
+++ Assets/Scripts/Settings/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioBox))]
+public class GameManager : MonoBehaviour
+{
+    static public GameManager Instance{get; private set;}
+
+    [SerializeField]
+    private AudioBox audioBox;
+
+    private void Awake()
+    {
+        if(Instance)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+
+        audioBox = GetComponent<AudioBox>();
+    }
+
+    private void Start()
+    {
+        audioBox.PlayLoop(SoundLoop.RoomSoundEffect);
+    }
+}
