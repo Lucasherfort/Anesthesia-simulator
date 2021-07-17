bccedfaf6efc9f58ef03f28ae5c261754fee8a53
lucasherfort <lucas.herfort@gmail.com>
2021-07-18 00:29:09 +0200

    Update script data

--- Assets/Scripts/Managers/DataRecorder.cs
+++ Assets/Scripts/Managers/DataRecorder.cs
@@ -1,11 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.IO;
+﻿using UnityEngine;
+using System;
 
 public class DataRecorder : MonoBehaviour
 {
-    private void Start()
+    static public DataRecorder Instance{get; private set;}
+
+    private void Awake()
+    {
+        if(Instance)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public void SaveData(TimeSpan DurationAnesthesia, int NbTouchNerve, int NbTouchVein, int NbTouchArtery)
     {
         if(!Application.isEditor) 
         {
@@ -15,8 +26,11 @@ public class DataRecorder : MonoBehaviour
             path = path + "/RecordedData/";
             System.IO.Directory.CreateDirectory(path);
             path = path + "RecordedData_" + System.DateTime.Now.ToString("dd-MM-yy") + "_" + System.DateTime.Now.ToString("HH-mm-ss") + ".txt";
-            
-            string report = "Date : "+System.DateTime.Now.ToString()+"\n\nDurée de l'anesthésie : 00:00\n\nNb nerfs touchés : 0\n\nNb veines touchéees : 0\n\nNb artères touchéees : 0";
+
+            string minute = string.Format("{0:D2}", DurationAnesthesia.Minutes);
+            string seconds = string.Format("{0:D2}", DurationAnesthesia.Seconds);
+
+            string report = "Date : "+System.DateTime.Now.ToString()+"\n\nDurée de l'anesthésie : "+minute+":"+seconds+"\n\nNb nerfs touchés : "+NbTouchNerve+"\n\nNb veines touchées : "+NbTouchVein+"\n\nNb artères touchées : "+NbTouchArtery;
           
             System.IO.File.WriteAllText(path, report);            
             
