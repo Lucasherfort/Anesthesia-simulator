5f4f467162f1d54ca25de05d4b057d909e05f03d
lucasherfort <lucas.herfort@gmail.com>
2021-07-18 00:03:27 +0200

    First version DataRecorder

---
+++ Assets/Scripts/Managers/DataRecorder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DataRecorder : MonoBehaviour
+{
+    private void Start()
+    {
+        if(!Application.isEditor) 
+        {
+            string path = Application.dataPath;
+            
+            path = path.Substring(0, path.LastIndexOf('/'));
+            path = path + "/RecordedData/";
+            System.IO.Directory.CreateDirectory(path);
+            path = path + "RecordedData_" + System.DateTime.Now.ToString("dd-MM-yy") + "_" + System.DateTime.Now.ToString("HH-mm-ss") + ".txt";
+            
+            string report = "Date : "+System.DateTime.Now.ToString()+"\n\nDurée de l'anesthésie : 00:00\n\nNb nerfs touchés : 0\n\nNb veines touchéees : 0\n\nNb artères touchéees : 0";
+          
+            System.IO.File.WriteAllText(path, report);            
+            
+        }
+    }
+}
