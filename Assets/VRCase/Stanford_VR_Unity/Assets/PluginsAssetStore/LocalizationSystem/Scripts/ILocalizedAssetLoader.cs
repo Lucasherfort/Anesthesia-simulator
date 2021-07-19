e3c99abf3c0ffb2caa96f913588cf2b34b1b5503
lucasherfort <lucas.herfort@gmail.com>
2021-07-19 15:48:19 +0200

    Clean project ^^

--- Assets/VRCase/Stanford_VR_Unity/Assets/PluginsAssetStore/LocalizationSystem/Scripts/ILocalizedAssetLoader.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-namespace SmartLocalization{
-	internal interface ILocalizedAssetLoader {
-		T LoadAsset<T>(string assetKey, string languageCode) where T : UnityEngine.Object;
-	}
-}
