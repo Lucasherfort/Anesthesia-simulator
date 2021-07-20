9e7a7f3f061ee34d690797226d4d02be01e91976
lucasherfort <lucas.herfort@gmail.com>
2021-07-21 00:36:46 +0200

    Add PostProcessing

---
+++ Assets/PostProcessing/TouchNerveEffect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[RequireComponent(typeof(Volume))]
+public class TouchNerveEffect : MonoBehaviour
+{
+    private Volume Volume;
+
+    private void Start()
+    {
+        Volume = GetComponent<Volume>();
+    }
+}
